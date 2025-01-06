78dae0a41dcbaed3f32ef157fad88c3fbfdedc32
roy <roysch33@gmail.com>
2025-01-06 12:25:15 +0200

    fixed network

--- Network.java
+++ Network.java
@@ -21,7 +21,10 @@ public class Network {
         users[2] = new User("Baz");
         userCount = 3;
     }
-
+    
+    public int getUserCount() {
+        return this.userCount;
+    }
     /** Finds in this network, and returns, the user that has the given name.
      *  If there is no such user, returns null.
      *  Notice that the method receives a String, and returns a User object. */
